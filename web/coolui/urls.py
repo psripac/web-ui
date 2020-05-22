8756f91d50c99e2d717b1be1ffec1f23da2e8fd4
psripac <psriprac@yahoo.com>
2020-05-22 16:15:57 -0700

    added navi func

--- web/coolui/urls.py
+++ web/coolui/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('project/', views.projectView, name='project'),
+    path('contact/', views.contactView, name='contact'),
     path('', views.indexView, name='index'),
 ]
--- web/coolui/views.py
+++ web/coolui/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def indexView(request):
     return render(request, 'coolui/index.html')
+
+def contactView(request):
+    return render(request, 'coolui/contact.html')
+
+def projectView(request):
+    return render(request, 'coolui/project.html')
