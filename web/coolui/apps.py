a6decf17d04a99bca4f95f8623f10cf278dd506e
psripac <psriprac@yahoo.com>
2020-05-21 21:32:43 -0700

    settings and url config

---
+++ web/coolui/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CooluiConfig(AppConfig):
+    name = 'coolui'
---
+++ web/coolui/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def indexView(request):
+    return render(request, 'coolui/index.html')
